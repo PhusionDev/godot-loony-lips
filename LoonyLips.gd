0a155b40b11c85293639e75834d1766f76c6db6e
Jared Hodgkins <phusiondev@gmail.com>
2020-12-27 18:06:28 -0500

    15. queue_free() and reload_current_scene()

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -6,16 +6,23 @@ var story = "Once upon a time someone called %s ate a %s flavored sandwich which
 
 onready var DisplayText = $VBoxContainer/DisplayText
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var GameButtonLabel = $VBoxContainer/HBoxContainer/TextureButton/Label
 
 func _ready():
-	DisplayText.text = "Welcome to Loony Lips! We're going to tell a story and have a wonderful time!"
+	GameButtonLabel.get_font("font").size = 40
+	DisplayText.text = "Welcome to Loony Lips! We're going to tell a story and have a wonderful time! "
+	PlayerText.grab_focus()
 	check_player_words_length()
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
 
 func _on_TextureButton_pressed():
-	add_to_player_words()
+	if is_story_done():
+		# reload scene
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
 func add_to_player_words():
 	player_words.append(PlayerText.text)
@@ -28,7 +35,7 @@ func is_story_done():
 
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_game()
 	else:
 		prompt_player()
 
@@ -37,3 +44,12 @@ func tell_story():
 
 func prompt_player():
 	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+
+func end_game():
+	PlayerText.queue_free()
+	change_button_text()
+	tell_story()
+
+func change_button_text():
+	GameButtonLabel.get_font("font").size = 20
+	GameButtonLabel.text = "new"
